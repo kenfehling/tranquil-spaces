173838c239464f302ecd47217bf566b41b208bd1
Ken Fehling <ken@androidideas.org>
2015-05-02 20:10:44 -0400

    Moved static assets to /public

--- server.js
+++ server.js
@@ -8,15 +8,19 @@
 
 var express = require('express');
 var favicon = require('serve-favicon');
+var path = require('path');
 
 var app = express();
-app.use(favicon(__dirname + '/favicon.ico'));
-app.use(express.static(__dirname));
+var root = path.join(__dirname, 'public');
+app.use(favicon(path.join(root, 'favicon.ico')));
+app.use(express.static(root));
+
+console.log(root);
 
 var router = express.Router();
 router.route('/').get(function(req, res) {
     "use strict";
-    res.sendFile('/index.html', { root: __dirname});
+    res.sendFile('/index.html', { root: root });
 });
 
 var port = process.env.PORT || 5000;
